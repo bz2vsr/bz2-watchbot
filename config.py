9f206ae0bf49d95700a01ce6dd28d8930faa9996
Sev Sunday <823864+sevsunday@users.noreply.github.com>
2025-01-23 23:15:32 -0600

    new steam ids to monitor list

--- config.py
+++ config.py
@@ -8,19 +8,22 @@ DISCORD_WEBHOOK_URL = os.getenv('DISCORD_WEBHOOK_URL')
 API_URL = "https://multiplayersessionlist.iondriver.com/api/1.0/sessions?game=bigboat:battlezone_combat_commander" 
 
 MONITORED_STEAM_IDS = [
-    "76561199653748651",  # Sev
-    "76561198058690608",  # JudgeGuns
-    "76561197974548434",  # VTrider
-    "76561198846500539",  # Xohm
     "76561198006115793",  # Domakus
+    "76561198846500539",  # Xohm
+    "76561199653748651",  # Sev
+    "76561197974548434",  # VTrider
+    "bz2Cyber",           # Cyber
+    "76561198825004088",  # Lamper
     "herpmcderperson",    # Herp
     "76561198088036138",  # dd
-    "76561198820311491",  # m.s 
-    "76561198825004088",  # Lamper
-    "76561198026325621",  # F9Bomber
-    "bz2Cyber",           # Cyber
-    # "XXX", # Sly
-    # "XXX", # Econchump
+    # "76561198820311491",  # m.s 
+    # "76561198026325621",  # F9Bomber
+    # "76561198058690608",  # JudgeGuns
+    # "76561198820311491",  # blue_banana
+    # "76561199732480793",  # XPi
+    # "76561198088149233",  # Muffin
+    # "running-roxas",      # Sly
+    # "76561198068133931",  # Econchump
     # "76561198064801924",  # HappyOtter
     # "76561198045619216",  # Zack
     # "76561197970538803",  # Graves
