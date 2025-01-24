8dea308afae357e1ec9c751d1726ad2d2300cae5
Sev Sunday <823864+sevsunday@users.noreply.github.com>
2025-01-24 02:26:59 -0600

    fix monitored steam ids
    not including players that often join non-bz2vet games

--- config.py
+++ config.py
@@ -10,24 +10,24 @@ API_URL = "https://multiplayersessionlist.iondriver.com/api/1.0/sessions?game=bi
 MONITORED_STEAM_IDS = [
     "76561198006115793",  # Domakus
     "76561198846500539",  # Xohm
-    "76561199653748651",  # Sev
     "76561197974548434",  # VTrider
     "bz2Cyber",           # Cyber
     "76561198825004088",  # Lamper
     "herpmcderperson",    # Herp
     "76561198088036138",  # dd
-    # "76561198820311491",  # m.s 
+    "76561198820311491",  # m.s 
+    "running-roxas",      # Sly
+    "76561198068133931",  # Econchump
     # "76561198026325621",  # F9Bomber
-    # "76561198058690608",  # JudgeGuns
     # "76561198820311491",  # blue_banana
+    # "76561198058690608",  # JudgeGuns
     # "76561199732480793",  # XPi
     # "76561198088149233",  # Muffin
-    # "running-roxas",      # Sly
-    # "76561198068133931",  # Econchump
     # "76561198064801924",  # HappyOtter
     # "76561198045619216",  # Zack
     # "76561197970538803",  # Graves
     # "76561198345909972",  # Vivify
+    # "76561199653748651",  # Sev
 ]
 
 CHECK_INTERVAL = 15
