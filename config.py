ec240a7391ad1a2bf10bb25af307acdbd34785cc
Sev Sunday <823864+sevsunday@users.noreply.github.com>
2025-02-23 14:28:17 -0600

    fix steam ids

--- config.py
+++ config.py
@@ -12,10 +12,10 @@ NOTIFICATION_TAG = "<@&1137622254621032500>"  # ID-based role ping for @BZ2Playe
 MONITORED_STEAM_IDS = [
     "76561198006115793",  # Domakus
     "76561198846500539",  # Xohm
-    "bz2Cyber",           # Cyber
-    "76561197962996353",    # Herp
-    "bzlolol",            # blue_banana
-    "running-roxas",      # Sly
+    "76561198824607769",  # Cyber
+    "76561197962996353",  # Herp
+    "76561198043392032",  # blue_banana
+    "76561198076339639",  # Sly
     "76561198820311491",  # m.s 
     # "76561197974548434",  # VTrider
     # "76561198068133931",  # Econchump
